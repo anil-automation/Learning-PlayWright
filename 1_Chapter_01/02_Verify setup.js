35719e366af9b28c3611e76a9f2c6749dc553da9
Anilkumar yara <anilkumaryara564@gmail.com>
2026-02-13 23:56:08 +0530

    Remove project files from repository

--- 1_Chapter_01/02_Verify setup.js
+++
@@ -1,9 +0,0 @@
-console.log(process.version);
-
-console.log(process.platform);
-
-console.log(process.arch);
-
-console.log("Node version", process.version);
-
-console.log("Node path", process.execPath);
